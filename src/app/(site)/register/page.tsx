4510e4d9eaeec542bed3f29bc0567d0f9676fb3b
lertnarit64070222 <lertnarittoedsateerasak@gmail.com>
2024-02-07 19:14:59 +0700

    Regis not comp

---
+++ src/app/(site)/register/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Register = () => {
+  return (
+    <div className="w-full bg-slate-500 h-[650px] mt-[50px] flex justify-center items-center">
+        <div className="w-[450px] h-[650px] outline ring-black rounded-md">
+            <h1 className="text-[24px] ml-[30px] mt-[15px] font-semibold">Create you Account</h1>
+            <div className="w-full bg-yellow-500 h-[450px] mt-[25px] flex justify-center">
+
+            </div>
+            <p className="text-center mt-[20px]">Do you already have an account? Login here!</p>
+            
+        </div>
+    </div>
+  );
+};
+
+export default Register;
