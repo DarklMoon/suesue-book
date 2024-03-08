77ff822fe7d1a1443c1817a20a1036a5c1c1708e
gamikasa <kraemankraeman@hotmail.com>
2024-03-08 12:38:21 +0700

    Update

--- src/components/page/CashPayComp.tsx
+++ src/components/page/CashPayComp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import React from "react";
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
