15937424a5a9b466d8c75c03df215f4979894702
lertnarit64070222 <lertnarittoedsateerasak@gmail.com>
2024-03-08 11:09:43 +0700

    add dat

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { fetchBooks } from "@/lib/data";
 
 
 export async function POST(request: NextRequest): Promise<any> {
@@ -7,6 +8,7 @@ export async function POST(request: NextRequest): Promise<any> {
     const book_name = (await data).get("book_name");
     const author = (await data).get("author");
     const description = (await data).get("description");
+    const price = (await data).get("price");
     try {
       if (picture) {
         console.log("aa")
@@ -21,41 +23,73 @@ export async function POST(request: NextRequest): Promise<any> {
   }
 
 export async function PUT(request: NextRequest): Promise<any> {
-    const data = request.formData();
-    const picture = (await data).get("picture");
-    const book_name = (await data).get("book_name");
-    const author = (await data).get("author");
-    const description = (await data).get("description");
-    try {
-      if (picture) {
-        console.log("aa")
-      }
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-    return NextResponse.json({
+  const data = request.formData();
+  const email = (await data).get("email")
+  const password = (await data).get("password")
 
-      });
+  const data_mydb = await fetchBooks();
+
+  // try {
+
+  //   const filteredUsers = mockUser.filter(
+  //     (user: { email: string }) => user.email === email
+  //   );
+
+  //   if (filteredUsers.length > 0) {
+  //     if(password){
+  //       const match = await bcrypt.compare(password.toString(), filteredUsers[0].password);
+  //       if (match) {
+  //         console.log(filteredUsers);
+  //         const { session, expires } = await login({
+  //           user_id: filteredUsers[0].user_id,
+  //           email: filteredUsers[0].email,
+  //           username: filteredUsers[0].username,
+  //           first_name: filteredUsers[0].first_name,
+  //           last_name: filteredUsers[0].last_name,
+  //           phone: filteredUsers[0].phone,
+  //           role: filteredUsers[0].role,
+  //         });
+
+  //         // console.log("Session&Expires: ", session, expires)
+
+  //         return NextResponse.json({
+  //           DATA_LOGIN: filteredUsers,
+  //           Session: session
+  //         });
+  //     }
+
+  //   } else {
+  //     console.log("Wrong Email or Password");
+  //     return NextResponse.json({
+  //       message: "Wrong Email or Password",
+  //     });
+  //   }}
+  //   else{
+  //     console.log("User not found");
+  //     return NextResponse.json({
+  //       message: "User not found",
+  //     });
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  //   throw error;
+  // }
+
+
   }
 
 export async function GET(request: NextRequest): Promise<any> {
-    const data = request.formData();
-    const picture = (await data).get("picture");
-    const book_name = (await data).get("book_name");
-    const author = (await data).get("author");
-    const description = (await data).get("description");
-    try {
-      if (picture) {
-        console.log("aa")
-      }
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+  try {
+    const data_mydb = await fetchBooks()
+    console.log("GET_METHOD")
     return NextResponse.json({
+      return : data_mydb
+    });
 
-      });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
   }
 
 export async function DELETE(request: NextRequest): Promise<any> {
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -12,3 +12,29 @@ export async function fetchProducts() {
     throw new Error("Failed to fetch revenue data.");
   }
 }
+
+export async function fetchBooks() {
+  try {
+    // Use straight quotes or backticks for the SQL query string
+    const response = await callProducts("SELECT * FROM BOOK", []);
+    const data = JSON.stringify(response);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch revenue data.");
+  }
+}
+
+export async function fetchPayment() {
+  try {
+    // Use straight quotes or backticks for the SQL query string
+    const response = await callProducts("SELECT * FROM PAYMENT", []);
+    const data = JSON.stringify(response);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch revenue data.");
+  }
+}
