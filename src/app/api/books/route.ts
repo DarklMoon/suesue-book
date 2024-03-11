4f403d3bfc1fa5d5464353bd20daf52431608688
DarklMoon <phuriwat757@gmail.com>
2024-03-11 14:52:03 +0700

    Refactor: fixBugForBuild

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -28,54 +28,6 @@ export async function PUT(request: NextRequest): Promise<any> {
   const email = (await data).get("email")
   const password = (await data).get("password")
 
-
-  // try {
-
-  //   const filteredUsers = mockUser.filter(
-  //     (user: { email: string }) => user.email === email
-  //   );
-
-  //   if (filteredUsers.length > 0) {
-  //     if(password){
-  //       const match = await bcrypt.compare(password.toString(), filteredUsers[0].password);
-  //       if (match) {
-  //         console.log(filteredUsers);
-  //         const { session, expires } = await login({
-  //           user_id: filteredUsers[0].user_id,
-  //           email: filteredUsers[0].email,
-  //           username: filteredUsers[0].username,
-  //           first_name: filteredUsers[0].first_name,
-  //           last_name: filteredUsers[0].last_name,
-  //           phone: filteredUsers[0].phone,
-  //           role: filteredUsers[0].role,
-  //         });
-
-  //         // console.log("Session&Expires: ", session, expires)
-
-  //         return NextResponse.json({
-  //           DATA_LOGIN: filteredUsers,
-  //           Session: session
-  //         });
-  //     }
-
-  //   } else {
-  //     console.log("Wrong Email or Password");
-  //     return NextResponse.json({
-  //       message: "Wrong Email or Password",
-  //     });
-  //   }}
-  //   else{
-  //     console.log("User not found");
-  //     return NextResponse.json({
-  //       message: "User not found",
-  //     });
-  //   }
-  // } catch (error) {
-  //   console.log(error);
-  //   throw error;
-  // }
-
-
   }
 
 export async function GET(request: NextRequest): Promise<any> {
